760b2ac9ec209dda4ee516c25046c2bce1ff286f
rom1577 <mailforgit1577@gmail.com>
2024-02-23 17:44:17 +0300

    Исправить 3_2_file.py

--- practice/3_2_file.py
+++ practice/3_2_file.py
@@ -8,25 +8,23 @@ def func3_2(a, b, path):
 
         count_rows = 0  # счётчик количества строк в файле
         with open(path + str(file) + ".txt", "r") as f:
-            try:
+            # try:
                 # для каждой строки в файле
                 for s in f:
-                    s.rstrip()  # уборка "\n" символов
-                    count_rows += 1
-                    element = float(s)
-                    sum += element
-            # обработка исключения, если данные файле были другого типа и выход из функции
-            except ValueError:
-                raise ValueError("Проверьте файл " + str(file) + ".txt на корректность типа содержимых данных")
-
-            # продолжение после блока try
-            else:
+                    try:
+                        s.rstrip()  # уборка "\n" символов
+                        count_rows += 1
+                        element = float(s)
+                        sum += element
+                    # обработка исключения, если данные файле были другого типа и выход из функции
+                    except ValueError:
+                        print("Проверьте файл " + str(file) + ".txt на корректность типа содержимых данных")
+                        continue
                 # проверка на то, чтобы количество заполненных строк в файле было ровно 3
                 if count_rows != 3:
-                    # генерация исключения и выход из функции
-                    raise IndexError("Количесво заполненных строк в файле " + str(file) + ".txt должно быть 3")
+                    print("Количесво заполненных строк в файле " + str(file) + ".txt должно быть 3")
     return sum
 
 # предполагается, что файлы находятся в одной директорииш
-print("Сумма равна = ", func3_2(1, 2, "/mnt/c/python_lesson/practice/"))
+print("Сумма равна = ", func3_2(1, 10, ""))
 
