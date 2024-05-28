5e1c10b86e772a7ba8afc02bfb70573c034086df
rom1577 <mailforgit1577@gmail.com>
2024-05-28 20:37:01 +0300

    add first_clean_code.py

---
+++ pythonProject/clean_code_lesson/first_clean_code.py
@@ -0,0 +1,38 @@
+'''
+type - weapon_type
+// тип оружия в классе оружия
+
+name - animal_name
+//  кличка жикотного
+
+distance - damage_distance_m
+//  дистанция поражения оружия в метрах
+
+skill - dwarf_skill
+//  навык Дварфа
+
+consumption - engine_consumption_per_100km_kgs
+//  расход двигателя на 100 км в кг/с
+
+speed - car_speed_kmh
+//  скорость машины в км/ч
+
+angle - head_angle_of_rotation_deg
+//  угол поворота головы в градусах
+
+emotion - animal_emotion
+//  эмоция животного
+
+pressure - pressure_in_wheel_atm
+//  давление в шинах в атмосферах
+
+s1 - input_string_for_concat_1
+//  первая входная строка для конкатенации
+
+list - list_for_asc_sorting
+//  список для сортировки по позрастанию
+
+cost - current_car_cost_usd
+//  текущая стоимость машины в долларах США
+'''
+
