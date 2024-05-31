831c2eb833688e979f4bbb694c178df43c801055
rom1577 <mailforgit1577@gmail.com>
2024-06-01 01:29:57 +0300

    add second_clean_code,py

---
+++ pythonProject/clean_code_lesson/second_clean_code.py
@@ -0,0 +1,44 @@
+'''
+6.1
+
+# Пусть есть класс автомобиль (Car)
+
+fuel - fuel_type_of_engine  # тип топлива двигателя автомобиля
+color - color_of_whole_car  # цвет всей машины
+year - year_of_release  # год выпуска машины
+price - price_of_new_car  # цена машины с завода
+wheels - number_of_wheels  # количество колес
+'''
+
+'''
+6.2
+
+prediction_value  # предсказанное значение в схеме предиктор-корректор
+index_of_hashtable  # индекс в хэш таблице
+list_bubble_sort_asc  # отсортированный пузырьком список по возрастанию
+query_string  # строка запроса в базу данных 
+'''
+
+'''
+6.3
+
+pressure_of_wheel_Pa - increment_pressure_Pa  # Например в классе Car есть поле pressure_of_wheel_Pa, а
+                                              # в методе необходимо использовать давление для изменения значения 
+                                              # давления в колесе. Для этого давление increment_pressure_Pa 
+                                              # в методе отражает свою функциональность.
+email_user - local_email_user  # email_user используется в качестве поля класса, local_email_user - в методе класса
+total_price_usd - total_pen_price_usd  # total_price_usd - используется в качестве параметра функции
+                                       # total_pen_price_usd - используется в качестве переменной вне функции 
+                                       # с расширенным контекстом
+'''
+
+'''
+6.4
+
+meet - get_meet_of_cow_kg  # сколько мясо дает корова
+size - size_of_ainmal_m3  # размер животного в м ** 3
+health - health_level_of_dwarf  # уровень здоровья дварфа
+a - number_for_calculate  # поле для расчетов в классе калькулятора
+speed - speed_of_car_ms  # скорость автомобиля в классе автомобиль в м/с
+'''
+
