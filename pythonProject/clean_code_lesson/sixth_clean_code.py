a700a596db72395da0b41b0053dbb22c63b8e4a8
rom1577 <mailforgit1577@gmail.com>
2024-06-12 20:13:01 +0300

    add sixth_clean_code.py

---
+++ pythonProject/clean_code_lesson/sixth_clean_code.py
@@ -0,0 +1,38 @@
+'''
+def take_name(self) - def get_name(self)
+# полчить имя в классе Animal, откуда понятно, что name это кличка животного
+
+def set_size_body(self) - def set_size_body(self)
+# устанавливается размер тела объекта типа Animal
+
+def raining() - def is_raining()
+# функция проверки дождит ли на улице
+
+def drawing() - def draw_triangle()
+# функция рисования треугольника
+
+def calculate_diameter_and_delete_row() - def calculate_diameter, def delete_row()
+# функция имеет излишнюю функциональность, из-за чего её имя увеличивается;
+# заменил на 2 разных функции
+
+def info(self) - def show_info(self)
+# метод применяется к классу Car, где отображается информация по автомобилю
+
+def add(self) - def add_contact(self)
+# метод добавления контакта в класс адресная книга
+
+def balance() - def check_balance()
+# функция проверки баланса
+
+def start(self) - def start_engine(self)
+# метод старта двигателя в классе Car
+
+def new_quantity(self) - def update_quantity(self)
+# метод обновления количества товара в классе Product
+
+def max_length() - def find_max_length()
+# функция нахождения максимальной длины
+
+def volume_of_cylinder(self) - def calculate_volume(self)
+# метод расчета объема в классе цилиндр
+'''
