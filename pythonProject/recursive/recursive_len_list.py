5454f61ed39942b7cd0652e3460d9e4aa33009b3
rom1577 <mailforgit1577@gmail.com>
2024-08-19 21:31:59 +0300

    add recursive_len_list.py V2

--- pythonProject/recursive/recursive_len_list.py
+++ pythonProject/recursive/recursive_len_list.py
@@ -1,7 +1,6 @@
 def calculate_length_of_list(list_of_numbers: list):
-    try:
-        list_of_numbers.pop(0)
-        return 1 + calculate_length_of_list(list_of_numbers)
-    except IndexError:
+    if not list_of_numbers:
         return 0
+    list_of_numbers.pop(0)
+    return 1 + calculate_length_of_list(list_of_numbers)
 
