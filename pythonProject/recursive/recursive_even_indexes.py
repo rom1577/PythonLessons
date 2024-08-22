eb7c8cd22ce1e637c599d79e73bfcecffe69011e
rom1577 <mailforgit1577@gmail.com>
2024-08-22 17:56:39 +0300

    add recursive_even_indexes.py V.3

--- pythonProject/recursive/recursive_even_indexes.py
+++ pythonProject/recursive/recursive_even_indexes.py
@@ -1,8 +1,7 @@
 def find_even_indexes(number_list: list, i=0):
     if i == len(number_list):
         return
-    if number_list[i] % 2 == 0:
-        print(i)
-    i += 1
-    return find_even_indexes(number_list, i)
+
+    print(number_list[i])
+    return find_even_indexes(number_list, i+2)
 
