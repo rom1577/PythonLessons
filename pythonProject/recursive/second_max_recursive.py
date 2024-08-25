483ed40dfd7ec80ead0fa4d3c8bca873fb4496f2
rom1577 <mailforgit1577@gmail.com>
2024-08-25 19:24:56 +0300

    add second_max_recursive.py V3

--- pythonProject/recursive/second_max_recursive.py
+++ pythonProject/recursive/second_max_recursive.py
@@ -1,36 +1,33 @@
-def find_second_max(list_of_numbers: list, first_max_element: int):
-    '''
-    Функция для расчета второго максимального элемента списка
-    '''
-    if len(list_of_numbers) == 1:
-        return list_of_numbers[0] if list_of_numbers[0] != first_max_element else None
-
-    second_max_element = find_second_max(list_of_numbers[1:], first_max_element)
-
-    if list_of_numbers[0] != first_max_element:
-        return max(list_of_numbers[0], second_max_element) if second_max_element is not None else list_of_numbers[0]
-    elif list_of_numbers.count(list_of_numbers[0]) > 1:
-        return first_max_element
-    else:
-        return second_max_element
-
-def find_first_max(list_of_numbers: list)->int:
-    '''
-    Функция для нахождения первого максимального элемента в списке
-    '''
-    max_element = list_of_numbers[0]
-    for i in range(1, len(list_of_numbers)):
-        if max_element < list_of_numbers[i]:
-            max_element = list_of_numbers[i]
-    return max_element
-
 def find_second_max_input(list_of_numbers: list):
     '''
     Функция вызова для нахождения второго максимального элемента в списке
     '''
     if len(list_of_numbers) < 2:
         return None
+    return find_second_max(list_of_numbers, list_of_numbers[0])[1]  # Определение второго максимального элемента
 
-    first_max = find_first_max(list_of_numbers)
-    return find_second_max(list_of_numbers, first_max)  # Определение второго максимального элемента
+def find_second_max(list_of_numbers: list, first_max_element: int)->int:
+    '''
+    Функция нахождения второго максимального элемента в списке
+    '''
+
+    # Опредлеение первого максимального числа при прямом ходе рекурсии
+    if list_of_numbers[0] > first_max_element:
+        second_max_element = first_max_element
+        first_max_element = list_of_numbers[0]
+    else:
+        second_max_element = list_of_numbers[0]
+
+    if len(list_of_numbers) == 1:
+        return first_max_element, second_max_element
+
+    first_max_element, second_max_element = find_second_max(list_of_numbers[1:], first_max_element)
+
+    # Опредлеение второго максимального числа при обратном ходе рекурсии
+    if list_of_numbers.count(first_max_element) != 1 and list_of_numbers.count(first_max_element) != 0:
+        second_max_element = first_max_element
+    elif list_of_numbers[0] > second_max_element and list_of_numbers[0] < first_max_element:
+        second_max_element = list_of_numbers[0]
+
+    return first_max_element, second_max_element
 
