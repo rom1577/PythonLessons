b7de9c3b4e5ecf9c6af93d660a7d5a4494c23ddd
rom1577 <mailforgit1577@gmail.com>
2024-08-26 19:02:31 +0300

    add second_max_recursive.py V5

--- pythonProject/recursive/second_max_recursive.py
+++ pythonProject/recursive/second_max_recursive.py
@@ -1,33 +1,24 @@
 def find_second_max_input(list_of_numbers: list):
-    '''
-    Функция вызова для нахождения второго максимального элемента в списке
-    '''
     if len(list_of_numbers) < 2:
         return None
-    return find_second_max(list_of_numbers, list_of_numbers[0])[1]  # Определение второго максимального элемента
 
-def find_second_max(list_of_numbers: list, first_max_element: int)->int:
-    '''
-    Функция нахождения второго максимального элемента в списке
-    '''
+    if list_of_numbers[0] > list_of_numbers[1]:
+        max1 = list_of_numbers[0]
+        max2 = list_of_numbers[1]
+    else:
+        max1 = list_of_numbers[1]
+        max2 = list_of_numbers[0]
+    return find_second_max(list_of_numbers[2:], max1, max2)
 
-    # Опредлеение первого максимального числа при прямом ходе рекурсии
-    if list_of_numbers[0] > first_max_element:
+def find_second_max(list_of_numbers: list, first_max_element: int, second_max_element: int)->int:
+    if len(list_of_numbers) == 0:
+        return second_max_element
+
+    if list_of_numbers[0] >= first_max_element:
         second_max_element = first_max_element
         first_max_element = list_of_numbers[0]
-    else:
+    elif list_of_numbers[0] > second_max_element:
         second_max_element = list_of_numbers[0]
 
-    if len(list_of_numbers) == 1:
-        return first_max_element, second_max_element
-
-    first_max_element, second_max_element = find_second_max(list_of_numbers[1:], first_max_element)
-
-    # Опредлеение второго максимального числа при обратном ходе рекурсии
-    if list_of_numbers.count(first_max_element) != 1 and list_of_numbers.count(first_max_element) != 0:
-        second_max_element = first_max_element
-    elif list_of_numbers[0] > second_max_element and list_of_numbers[0] < first_max_element:
-        second_max_element = list_of_numbers[0]
-
-    return first_max_element, second_max_element
+    return find_second_max(list_of_numbers[1:], first_max_element, second_max_element)
 
