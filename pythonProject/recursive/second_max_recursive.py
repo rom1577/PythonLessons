db0becc5afbbb616f3c38e86f8722d24e23a4866
rom1577 <mailforgit1577@gmail.com>
2024-08-28 15:13:07 +0300

    add second_max_recursive.py V7

--- pythonProject/recursive/second_max_recursive.py
+++ pythonProject/recursive/second_max_recursive.py
@@ -3,22 +3,24 @@ def find_second_max_input(list_of_numbers: list):
         return None
 
     if list_of_numbers[0] > list_of_numbers[1]:
-        max1 = list_of_numbers[0]
-        max2 = list_of_numbers[1]
+        max1 = list_of_numbers.pop(0)
+        max2 = list_of_numbers.pop(0)
     else:
-        max1 = list_of_numbers[1]
-        max2 = list_of_numbers[0]
+        max2 = list_of_numbers.pop(0)
+        max1 = list_of_numbers.pop(0)
     return find_second_max(list_of_numbers, max1, max2)
 
-def find_second_max(list_of_numbers: list, first_max_element: int, second_max_element: int, i=2)->int:
-    if i == len(list_of_numbers):
+def find_second_max(list_of_numbers: list, first_max_element: int, second_max_element: int)->int:
+    if len(list_of_numbers) == 0:
         return second_max_element
 
-    if list_of_numbers[i] >= first_max_element:
+    cur_element = list_of_numbers.pop(0)
+
+    if cur_element >= first_max_element:
         second_max_element = first_max_element
-        first_max_element = list_of_numbers[i]
-    elif list_of_numbers[i] > second_max_element:
-        second_max_element = list_of_numbers[i]
-    i +=1
-    return find_second_max(list_of_numbers, first_max_element, second_max_element,i)
+        first_max_element = cur_element
+    elif cur_element > second_max_element:
+        second_max_element = cur_element
+
+    return find_second_max(list_of_numbers, first_max_element, second_max_element)
 
